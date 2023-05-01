1dfecf84eaa23d25420e4f5c9421c23ff8c56be9
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-05-01 16:55:25 +0900

    Initial commit

---
+++ src/test/java/com/hana/oauthstudy/OauthStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.oauthstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
